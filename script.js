b2afe1c7a2a4195dad49885cb7b88faed1e7845d
Jake Choi <62556343+Jake-Choi9233@users.noreply.github.com>
2026-01-31 03:01:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿const revealItems = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+revealItems.forEach((item) => observer.observe(item));
